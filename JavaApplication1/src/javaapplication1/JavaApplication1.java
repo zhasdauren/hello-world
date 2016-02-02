107c0aab12d15d733a9519c46e7342e5b6cea34d
zhasdauren <zhasdauren@users.noreply.github.com>
2016-02-02 13:44:33 +0600

    Java file updated
    Added print.

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -17,6 +17,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello!");
     }
     
 }
