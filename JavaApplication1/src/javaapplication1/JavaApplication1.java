96d1ddcede9f42fcb6c00857e49c959cd047bde0
lab <lab@ECO-LAB-24.sdu.local>
2016-02-02 13:54:00 +0600

    Java file added

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -18,6 +18,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello!");
+        System.out.println("World!");
     }
     
 }
